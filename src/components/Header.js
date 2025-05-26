68012c45ffabfcb7608c636b5d4dda0e3f09b8c1
kalyansaruvu <saruvukalyan19@gmail.com>
2025-05-26 13:30:33 +0530

    logo and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
-import {Link, NavLink} from "react-router-dom"
+import { NavLink} from "react-router-dom"
 import logo from "../assets/logo.png"
 export const Header = () => {
   return (
     <div>
-    <header>
-    <img   src={logo} alt="logo" /><h1 > MOVIE FLIX</h1>
-    <NavLink className="navbar">
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/Populars"className="nav-link">Populars</Link>
-      <Link to="/TopRated"className="nav-link">TopRated</Link>
-      <Link to="/UpComing"className="nav-link">UpComing</Link>
-    </NavLink>
-  </header>
+   <header>
+  <div className="header-container">
+    <div className="branding">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="title">MOVIE FLIX</h1>
+    </div>
+
+    <nav className="navbar">
+      <NavLink to="/" className="nav-link">Home</NavLink>
+      <NavLink to="/Populars" className="nav-link">Populars</NavLink>
+      <NavLink to="/TopRated" className="nav-link">TopRated</NavLink>
+      <NavLink to="/UpComing" className="nav-link">UpComing</NavLink>
+    </nav>
+  </div>
+</header>
   </div>
   )
 }
